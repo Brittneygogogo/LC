121ba69957f0035c6955087710fb579bcd264c7f
shixt_ok@126.com <shixt1994>
2021-03-09 22:20:32 +0800

    edit .gitignore

--- Sort/quick.py
+++ Sort/quick.py
@@ -60,35 +60,53 @@ def quick_sort2(ls):
     right_res = quick_sort2(right_ls)
 
     return left_res + [mid_val] + right_res
-
-def quick_sort3(L):
-    return q_sort(L, 0, len(L) - 1)
-
-def q_sort(L, left, right):
-    if left < right:
-        pivot = Partition(L, left, right)
-
-        q_sort(L, left, pivot - 1)
-        q_sort(L, pivot + 1, right)
-    return L
-
-def Partition(L, left, right):
-    pivotkey = L[left]
-
-    while left < right:
-        while left < right and L[right] >= pivotkey:
-            right -= 1
-        L[left] = L[right]
-        while left < right and L[left] <= pivotkey:
-            left += 1
-        L[right] = L[left]
-
-    L[left] = pivotkey
-    return left
-
-L = [5, 9, 1, 11, 6, 7, 2, 4]
+#
+# def quick_sort3(L):
+#     return q_sort(L, 0, len(L) - 1)
+#
+# def q_sort(L, left, right):
+#     if left < right:
+#         pivot = Partition(L, left, right)
+#
+#         q_sort(L, left, pivot - 1)
+#         q_sort(L, pivot + 1, right)
+#     return L
+#
+# def Partition(L, left, right):
+#     pivotkey = L[left]
+#
+#     while left < right:
+#         while left < right and L[right] >= pivotkey:
+#             right -= 1
+#         L[left] = L[right]
+#         while left < right and L[left] <= pivotkey:
+#             left += 1
+#         L[right] = L[left]
+#
+#     L[left] = pivotkey
+#     return left
+#
+# L = [5, 9, 1, 11, 6, 7, 2, 4]
 
 # print(quick_sort3(L))
+
+# def quick_sort3(array, left, right):
+#     if left < right:
+#         q = partition(array, left, right)
+#         quick_sort3(array, left, q - 1)
+#         quick_sort3(array, q + 1, right)
+#
+# def partition(array, left, right):
+#     x = array[right]
+#     i = left - 1
+#     for j in range(left, right):
+#         if array[j] <= x:
+#             i += 1
+#             array[i], array[j] = array[j], array[i]
+#     array[i + 1], array[right] = array[right], array[i + 1]
+#     return i + 1
+#
+
 
 
 if __name__ == "__main__":
@@ -103,4 +121,4 @@ if __name__ == "__main__":
     res2 = quick_sort2(ls)
     print("quick sort2：", res2)
     print("-" * 50)
-    print("quick sort3：", quick_sort3(ls))
+    print("quick sort3：", quick_sort3(ls, 0, len(ls)-1))
