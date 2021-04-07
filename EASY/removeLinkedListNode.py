a168beb21552dbe093b1d60c59c140dc7d250030
shixt_ok@126.com <shixt1994>
2021-04-07 20:44:43 +0800

    work

--- EASY/removeLinkedListNode.py
+++ EASY/removeLinkedListNode.py
@@ -1,30 +1,29 @@
-# '''
-# 删除节点为val的所有节点
-# '''
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-#
-# class Solution:
-#     def removeElements(self, head: ListNode, val: int) -> ListNode:
-#         sentinel = ListNode(0)
-#         sentinel.next = head
-#
-#         prev, curr = sentinel, head
-#         while curr:
-#             if curr.val == val:
-#                 prev.next = curr.next
-#             else:
-#                 prev = curr
-#                 curr = curr.next
-#
-#         return sentinel.next
-#
-#
-#
-# s = Solution()
-# root = ListNode(1)
-# root.next = ListNode(1)
-# root.next.next = ListNode(1)
-# print(s.removeElements(root, 1))
+'''
+删除节点为val的所有节点
+'''
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        sentinel = ListNode(0)
+        sentinel.next = head
+
+        prev, curr = sentinel, head
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+
+        return sentinel.next
+
+
+s = Solution()
+root = ListNode(1)
+root.next = ListNode(1)
+root.next.next = ListNode(1)
+print(s.removeElements(root, 1))
--- MEDIUM/hasCycle.py
+++ MEDIUM/hasCycle.py
@@ -1,3 +1,10 @@
+
+class ListNode:
+    def __init__(self, x, next):
+        self.val = x
+        self.next = next
+
+
 class Solution:
     def hasCycle(self, head) -> bool:
         if not head or not head.next:
@@ -11,6 +18,28 @@ class Solution:
                 return False
             slow = slow.next
             fast = fast.next.next
-
         return True
 
+
+class Solution:
+    def detectCycle(self, head: ListNode):
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                fast = head
+                while slow != fast:
+                    fast = fast.next
+                    slow = slow.next
+                return fast.val
+
+
+
+x = ListNode(2, None)
+head = ListNode(0, ListNode(1, x))
+x.next = ListNode(3, x)
+
+s = Solution()
+# print(s.hasCycle(head))
+print(s.detectCycle(head))
